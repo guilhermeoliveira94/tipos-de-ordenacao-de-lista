7aefc914da8a14d25c2bae87665010c9ba7191f3
Guilherme de Oliveira <84920918+guilhermeoliveira94@users.noreply.github.com>
2021-09-06 01:24:46 -0300

    Add files via upload

---
+++ Tipos de Ordenação de Lista.py
@@ -0,0 +1,11 @@
+lista = ['Cláudio', 'José', 'Maria', 'Beltrano', 'João', 'Fulano', 'Ciclano']
+print("Lista original: \n",lista)
+
+lista.reverse()
+print("Lista invertida: \n",lista)
+
+lista.sort()
+print("Lista ordenada em ordem Ascendente: \n",lista)
+
+lista.sort(reverse=True)
+print("Lista ordenada em ordem Descendete: \n",lista)
